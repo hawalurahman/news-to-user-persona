a8810786cb5e46ad5d74618ffcdd70ea00971e34
Halim <halim.wildan.awalurahman-2019@fst.unair.ac.id>
2022-10-05 11:02:49 +0700

    checkpoint

--- NewsToUserPersona/createUserPersona/views.py
+++ NewsToUserPersona/createUserPersona/views.py
@@ -121,6 +121,10 @@ def createUserPersona(group_of_sentences):
 def preprocessingWithSpacy(input):
     doc = nlp(input)
 
+    # menggunakan lemmatized form
+    # lemmatized_words = " ".join([token.lemma_ for token in doc])
+    # doc = nlp(lemmatized_words)
+
     # mencari pattern goals
     matcher = Matcher(nlp.vocab)
     pattern = [{'POS': 'VERB'},
